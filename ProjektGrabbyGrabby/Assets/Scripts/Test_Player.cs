2cf8a7ee8a02d86db3fe4b3f04740e2123c95b98
YuuVR <leyuudachi@hotmail.com>
2020-04-07 16:38:13 +0200

    Update: working on String release

--- ProjektGrabbyGrabby/Assets/Scripts/Test_Player.cs
+++ ProjektGrabbyGrabby/Assets/Scripts/Test_Player.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class Test_Player : MonoBehaviour
 {
+    private SpringJoint2D spring;
+    private float releaseDelay;
+    bool whileTouch = false;
+
+    private void Awake()
+    {
+        spring = GetComponent<SpringJoint2D>();
+
+        releaseDelay = 1 / (spring.frequency * 4);
+    }
     void Update()
     {
+        //TOUCHSCREEN FUNCTION / TRANSFORM PLAYER IF TOUCH SCREEN//
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
@@ -13,6 +24,18 @@ public class Test_Player : MonoBehaviour
             touchPosition.z = 0f;
             transform.position = touchPosition;
         }
+
+        //CUT SPRING / IF BUTTONS IS NOT TOUCHED ANYMORE SPRING RELEASE//
+        if (whileTouch)
+        {
+            StartCoroutine(Release());
+        }
+    }
+
+    private IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseDelay);
+        spring.enabled = false;
     }
 
 }
