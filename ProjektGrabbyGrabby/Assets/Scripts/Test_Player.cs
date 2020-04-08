8c98ee321219c990c32d2fcab9db09c20351a705
YuuVR <leyuudachi@hotmail.com>
2020-04-08 11:19:36 +0200

    Added: Ball can YEEEEEEEEEET

--- ProjektGrabbyGrabby/Assets/Scripts/Test_Player.cs
+++ ProjektGrabbyGrabby/Assets/Scripts/Test_Player.cs
@@ -5,31 +5,57 @@ using UnityEngine;
 public class Test_Player : MonoBehaviour
 {
     private SpringJoint2D spring;
+    private Rigidbody2D rigid;
     private float releaseDelay;
     bool whileTouch = false;
+    public Vector2 tPosition;
+    public Touch touch;
 
     private void Awake()
     {
         spring = GetComponent<SpringJoint2D>();
+        rigid = GetComponent<Rigidbody2D>();
 
         releaseDelay = 1 / (spring.frequency * 4);
     }
-    void Update()
+    public void Update()
     {
         //TOUCHSCREEN FUNCTION / TRANSFORM PLAYER IF TOUCH SCREEN//
-        if (Input.touchCount > 0)
+        /*if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
             Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
             touchPosition.z = 0f;
             transform.position = touchPosition;
-        }
+        } */
 
-        //CUT SPRING / IF BUTTONS IS NOT TOUCHED ANYMORE SPRING RELEASE//
+        touch = Input.GetTouch(0);
+        Debug.Log(touch.position);
+
         if (whileTouch)
         {
-            StartCoroutine(Release());
+            DragBall();
         }
+    }
+    private void DragBall()
+    {
+        Vector2 tPosition = Camera.main.ScreenToWorldPoint(touch.position);
+        rigid.position = tPosition;
+    }
+    //CUT SPRING / IF BUTTONS IS NOT TOUCHED ANYMORE SPRING RELEASE//
+    private void OnMouseDown()
+    {
+        whileTouch = true;
+        Debug.Log("istouched");
+        rigid.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        whileTouch = false;
+        Debug.Log("isnottouched");
+        rigid.isKinematic = false;
+        StartCoroutine(Release());
     }
 
     private IEnumerator Release()
