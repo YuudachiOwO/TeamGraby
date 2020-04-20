4c4c1706f4da45de50cb7c9f815817c2ad0b220a
YuuVR <leyuudachi@hotmail.com>
2020-04-20 10:50:13 +0200

    Level Gen

---
+++ ProjektGrabbyGrabby/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+    [SerializeField] private Transform levelPart_Start;
+    [SerializeField] private Transform levelPart_1;
+   
+
+    private void Awake()
+    {
+        SpawnLevelPart(new Vector3(38, -4));
+    }
+    private void SpawnLevelPart(Vector3 spawnPosition)
+    {
+        Instantiate(levelPart_1, spawnPosition, Quaternion.identity);
+    }
+}
