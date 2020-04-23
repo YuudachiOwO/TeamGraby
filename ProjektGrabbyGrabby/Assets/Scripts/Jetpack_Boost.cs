4d8a7e326357e661100873e5765166ed5da13507
Dennis <dmaier2406@gmail.com>
2020-04-23 12:23:00 +0200

    JETPACK BOIIIIIII (BUILD TESTING NEEDED)
    Leggo, Jetpack Working in BUILD ONLY

---
+++ ProjektGrabbyGrabby/Assets/Scripts/Jetpack_Boost.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jetpack_Boost : MonoBehaviour
+{
+    public Rigidbody2D playerRB;
+    public bool jetpackBurning;
+    public float boosterStrengthUp = 1f;
+    public float boosterStrengthSide = 1f;
+
+    
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+        jetpackBurning = false;
+    }
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            jetpackBurning = true;
+        }
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            jetpackBurning = false;
+        }
+
+        if (jetpackBurning)
+        {
+            playerRB.AddForce(new Vector2(boosterStrengthSide, boosterStrengthUp), ForceMode2D.Force);
+        }
+        else 
+        {
+            playerRB.AddForce(new Vector2(0f, 0f), ForceMode2D.Force);
+        }
+    }
+}
