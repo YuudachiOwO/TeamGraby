2ea3da680f7838faa58e9742eefbe3a443d5704a
YuuVR <leyuudachi@hotmail.com>
2020-05-06 13:20:45 +0200

    Replaced_Placeholder and added Main Menu

---
+++ ProjektGrabbyGrabby/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public void menu()
+    {
+        SceneManager.LoadScene("Menu"); //LoadScene when button is clicked
+    }
+
+    public void start()
+    {
+        SceneManager.LoadScene("Teste_scene_(Kevin)"); //LoadScene when button is clicked
+    }
+
+    public void quitGame()
+    {
+        Application.Quit(); //Quit Game button
+        UnityEditor.EditorApplication.isPlaying = false; //Quit Game in Unity Editor
+    }
+
+}
