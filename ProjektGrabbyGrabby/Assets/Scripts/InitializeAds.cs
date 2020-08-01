f7bfda578fba5cced72181971655daa302a0f479
Dennis <dmaier2406@gmail.com>
2020-08-02 01:55:59 +0200

    WERBUNG FUNKTIONIERT!

--- ProjektGrabbyGrabby/Assets/Scripts/InitializeAds.cs
+++ ProjektGrabbyGrabby/Assets/Scripts/InitializeAds.cs
@@ -1,32 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using GoogleMobileAds.Api;
 
 public class InitializeAds : MonoBehaviour
 {
+
+    
     private BannerView bannerView;
     void Start()
     {
         MobileAds.Initialize(InitializationStatus => { });
+        Debug.Log("HELLO WORLD");
         this.RequestBanner();
     }
 
     private void RequestBanner()
     {
 #if UNITY_ANDROID
-            string adUnitId = "ca-app-pub-3575984459592926/8599929260";
+            string adUnitId = "ca-app-pub-3940256099942544/6300978111";
 #elif UNITY_IPHONE
             string adUnitId = "ca-app-pub-3940256099942544/2934735716";
 #else
             string adUnitId = "unexpected_platform";
 #endif
 
+        Debug.Log("SomeBody Once Told me");
         // Create a 320x50 banner at the top of the screen.
-        this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Center);
+        this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+        this.bannerView.OnAdLoaded += this.HandleOnAdLoaded;
+        this.bannerView.OnAdFailedToLoad += this.HandleOnAdFailedToLoad;
         // Create an empty ad request.
         AdRequest request = new AdRequest.Builder().Build();
         // Load the banner with the request.
         this.bannerView.LoadAd(request);
+    }
+
+    public void HandleOnAdLoaded(object sender, EventArgs args)
+    {
+        Debug.Log("Event received");
+        bannerView.Show();
+    }
+
+    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
+                            + args.Message);
     }
 }
